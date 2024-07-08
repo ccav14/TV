b911993b254db0fc64cc16f862ba1bc6130b605f
ccav14 <1044747052@qq.com>
2024-07-08 17:42:42 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,42 @@
+open_update = True
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "CCTV-1",
+    "CCTV-2",
+    "CCTV-3",
+    "CCTV-4",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-6",
+    "CCTV-7",
+    "CCTV-8",
+     "CCTV-13",
+     "广东珠江",
+    "广东体育",
+    "广东卫视",
+    "大湾区卫视",
+    "浙江卫视",
+    "湖南卫视",
+     "东方卫视",
+]
+open_online_search = True
+favorite_page_num = 5
+default_page_num = 3
+urls_limit = 15
+open_sort = True
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
+open_subscribe = True
+subscribe_urls = [
+    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/o_cn.txt",
+    "https://m3u.ibert.me/txt/j_iptv.txt",
+]
+open_multicast = True
+region_list = ["all"]
+open_proxy = True
