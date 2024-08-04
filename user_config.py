6c4bbdd0a49c84dd609b4784719d983751a0d34c
ccav14 <1044747052@qq.com>
2024-08-04 18:41:29 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -1,4 +1,5 @@
 open_update = True
+open_use_old_result = True
 source_file = "user_demo.txt"
 final_file = "user_result.txt"
 favorite_list = [
@@ -24,6 +25,7 @@ open_online_search = True
 favorite_page_num = 5
 default_page_num = 3
 urls_limit = 15
+open_keep_all = False
 open_sort = True
 response_time_weight = 0.5
 resolution_weight = 0.5
@@ -40,3 +42,10 @@ subscribe_urls = [
 open_multicast = True
 region_list = ["all"]
 open_proxy = True
+open_driver = True
+open_use_old_result = True
+
+
+
+
+
